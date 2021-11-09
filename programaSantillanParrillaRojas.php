bb65e802193efeb6e91982c78ad0edc1082212e9
emilianoparrilla <emilianoparrilla93@gmail.com>
2021-11-09 13:08:19 -0300

    Agrego la función mostrarJuego

--- programaSantillanParrillaRojas.php
+++ programaSantillanParrillaRojas.php
@@ -5,8 +5,8 @@ include_once("tateti.php");
 /***** DATOS DE LOS INTEGRANTES *******/
 /**************************************/
 
-/* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
-/* Santillan, Juan Ignacio. Legajo: 2691. Carrera: Tecnicatura Universitaria en Desarrollo Web. mail: juan.santillan@est.fi.uncoma.edu.ar 
+/* Apellido. Nombre. Legajo. Carrera. mail. Usuario Github */
+/* Santillan. Juan Ignacio. Legajo: 2691. Carrera: Tecnicatura Universitaria en Desarrollo Web. mail: juan.santillan@est.fi.uncoma.edu.ar 
 Usuario GitHub: juani029
 */
 /* . Legajo: . Carrera: Tecnicatura Universitaria en Desarrollo Web. mail:  
@@ -30,21 +30,21 @@ Usuario GitHub:
 function cargarJuegos(){
     // array $coleccionJuegos
     $coleccionJuegos = [];
-    $coleccionJuegos[0] = ["jugadorCruz"=> "Arturo", "jugadorCirculo" => "Rodrigo", "puntosCruz"=> 4, "puntosCirculo" => 0];
-    $coleccionJuegos[1] = ["jugadorCruz"=> "Marcelo", "jugadorCirculo" => "Maria", "puntosCruz"=> 0, "puntosCirculo" => 3];
-    $coleccionJuegos[2] = ["jugadorCruz"=> "Roberto", "jugadorCirculo" => "Camila", "puntosCruz"=> 1, "puntosCirculo" => 1];
-    $coleccionJuegos[3] = ["jugadorCruz" => "Rodrigo", "jugadorCirculo" => "Marcelo", "puntosCruz" => 2, "puntosCirculo" => 0];
-    $coleccionJuegos[4] = ["jugadorCruz" => "Camila", "jugadorCirculo" => "Arturo", "puntosCruz" => 0, "puntosCirculo" => 5];
-    $coleccionJuegos[5] = ["jugadorCruz"=> "Maria", "jugadorCirculo" => "Cecilia", "puntosCruz"=> 1, "puntosCirculo" => 1];
-    $coleccionJuegos[6] = ["jugadorCruz"=> "Aldana", "jugadorCirculo" => " Daiana", "puntosCruz" => 0, "puntosCirculo" => 2];
-    $coleccionJuegos[7] = ["jugadorCruz" => "Cecilia", "jugadorCirculo" => "Aldana", "puntosCruz" => 4, "puntosCirculo" => 0];
-    $coleccionJuegos[8] = ["jugadorCruz"=> "Daiana", "jugadorCirculo" => "Marcelo", "puntosCruz"=> 1, "puntosCirculo" => 1];
-    $coleccionJuegos[9] = ["jugadorCruz" => "Rodrigo", "jugadorCirculo" => "Arturo", "puntosCruz" => 0, "puntosCirculo" => 2];
+    $coleccionJuegos[0] = ["jugadorCruz"=> "Arturo". "jugadorCirculo" => "Rodrigo". "puntosCruz"=> 4. "puntosCirculo" => 0];
+    $coleccionJuegos[1] = ["jugadorCruz"=> "Marcelo". "jugadorCirculo" => "Maria". "puntosCruz"=> 0. "puntosCirculo" => 3];
+    $coleccionJuegos[2] = ["jugadorCruz"=> "Roberto". "jugadorCirculo" => "Camila". "puntosCruz"=> 1. "puntosCirculo" => 1];
+    $coleccionJuegos[3] = ["jugadorCruz" => "Rodrigo". "jugadorCirculo" => "Marcelo". "puntosCruz" => 2. "puntosCirculo" => 0];
+    $coleccionJuegos[4] = ["jugadorCruz" => "Camila". "jugadorCirculo" => "Arturo". "puntosCruz" => 0. "puntosCirculo" => 5];
+    $coleccionJuegos[5] = ["jugadorCruz"=> "Maria". "jugadorCirculo" => "Cecilia". "puntosCruz"=> 1. "puntosCirculo" => 1];
+    $coleccionJuegos[6] = ["jugadorCruz"=> "Aldana". "jugadorCirculo" => " Daiana". "puntosCruz" => 0. "puntosCirculo" => 2];
+    $coleccionJuegos[7] = ["jugadorCruz" => "Cecilia". "jugadorCirculo" => "Aldana". "puntosCruz" => 4. "puntosCirculo" => 0];
+    $coleccionJuegos[8] = ["jugadorCruz"=> "Daiana". "jugadorCirculo" => "Marcelo". "puntosCruz"=> 1. "puntosCirculo" => 1];
+    $coleccionJuegos[9] = ["jugadorCruz" => "Rodrigo". "jugadorCirculo" => "Arturo". "puntosCruz" => 0. "puntosCirculo" => 2];
     return($coleccionJuegos);
 }
 
 /**
- *Este módulo muestra en pantalla el menú de opciones, solicita al usuario que elija una opción, si es válida retorna la opción y sino repite hasta que la opción válida
+ *Este módulo muestra en pantalla el menú de opciones. solicita al usuario que elija una opción. si es válida retorna la opción y sino repite hasta que la opción válida
  *@return int	
  */
 function seleccionarOpcion(){
@@ -61,6 +61,18 @@ function seleccionarOpcion(){
 		$opcion = trim(fgets(STDIN));
 } while ($opcion < 1 || $opcion > 7);
 	return($opcion);
+}
+
+/**
+ * Este módulo recibe un número de juego como parámetro y si el juego existe. imprime en pantalla los datos del mismo. Si no existe. repite el proceso hasta que se ingrese un número válido.
+ * @param int $nroJuego
+ */
+function mostrarJuego ($nroJuego){
+    echo "**********************";
+    echo "Juego TATETI: ". $nroJuego. " (" . $coleccionJuegos["resultado"] . ")
+    echo ("Jugador X: " . $coleccionJuegos["jugadorCruz"]. "obtuvo " . $coleccionJuegos["puntosCruz"] . " puntos";
+    echo ("Jugador O: " . $coleccionJuegos["jugadorCirculo"]. "obtuvo " . $coleccionJuegos["puntosCirculo"] . " puntos";
+    echo "**********************";
 }
 
 
